bc65f4f00d07b3512611cac249e0c9edb41fe3c2
Gustavo Niemeyer <gustavo@niemeyer.net>
2013-12-03 00:02:39 -0200

    Updated test case with newer versions.

--- sort_test.go
+++ sort_test.go
@@ -5,6 +5,7 @@ import (
 	"math/rand"
 	"sort"
 	"testing"
+	"time"
 )
 
 func Test(t *testing.T) {
@@ -16,18 +17,25 @@ type S struct{}
 var _ = Suite(S{})
 
 func (S) TestVersionOrder(c *C) {
+	//rand.Seed(time.Now().UnixNano())
 	versions := []string{
+		"1.2",
+		"1.2rc5",
+		"1.2rc4",
+		"1.2rc3",
+		"1.2rc2",
+		"1.2rc1",
+		"1.1.2",
 		"1.1.1",
 		"1.1",
 		"1.1rc3",
 		"1.1rc2",
 		"1.1rc1",
 		"1.1beta2",
-		"1.0.10",
+		"1.1beta1",
 		"1.0.3",
+		"1.0.2",
 		"1.0.1",
-		"1.0",
-		"1.0rc1",
 	}
 
 	for perm := 0; perm < 1000; perm++ {
