995f002b400325f9b9df0e91e0cd13fced920920
Joseph Glanville <jpg@jpg.id.au>
2016-01-27 04:10:33 +0000

    code.google.com has been shutdown
    Signed-off-by: Joseph Glanville <jpg@jpg.id.au>

--- cmd/godeb/main.go
+++ cmd/godeb/main.go
@@ -184,7 +184,6 @@ type tarballSource struct {
 }
 
 var tarballSources = []tarballSource{
-	{"https://code.google.com/p/go/downloads/list?can=1&q=linux", "//a/@href[contains(., 'go.googlecode.com')]"},
 	{"http://golang.org/dl/", "//a/@href[contains(., 'storage.googleapis.com/golang/')]"},
 }
 
